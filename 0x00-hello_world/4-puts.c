774355bc9beb38b4fd0967d2047d9b212fd3fdc8
jaycode2023 <jaycodeworld@gmail.com>
2023-02-16 09:29:17 -0800

    cfile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - a programe that prints a line with puts function
- * Return: 0 (success)
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
