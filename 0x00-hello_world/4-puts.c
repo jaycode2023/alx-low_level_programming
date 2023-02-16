a7777f8dce8521ed121170abcd8d8d431a7b7c45
jaycode2023 <jaycodeworld@gmail.com>
2023-02-16 09:12:03 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - a programe that prints a line with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
