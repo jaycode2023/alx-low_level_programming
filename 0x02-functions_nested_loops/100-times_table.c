d35f20fb50a18f700a728c80b05595ce3d914d54
jaycode2023 <jaycodeworld@gmail.com>
2023-02-22 12:49:57 -0800

    100-time

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,8 +2,8 @@
 
 /**
  * print _times_table - prints the times table of the input,
- * starting with 0,
- * @n: The value of the times table to be printed
+ * starting with 0.
+ * @n: The value of the times table to be printed.
  */
 
 void print_times_table(int n)
