ce6a20c4296ddd2efee2486b9a9d2cd1913a071e
jaycode2023 <jaycodeworld@gmail.com>
2023-02-22 02:29:51 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,8 @@
  * @n:The number of which the sign will be printed
  * Return: 1 for possitive num, -1 for negative num or zero for anything else
  */
-int print_sign(int n0
+
+int print_sign(int n)
 
 {
 	if (n > 0)
