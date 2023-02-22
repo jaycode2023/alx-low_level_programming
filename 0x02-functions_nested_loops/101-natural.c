6755029fe9efa0db84411f656df2d64b5cd707d4
jaycode2023 <jaycodeworld@gmail.com>
2023-02-22 13:59:32 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int i, z  = 0;
+
+	while  (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += 1;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
