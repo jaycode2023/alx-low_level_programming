08a62e758b195cf45975c883f4619df7ee013069
jaycode2023 <jaycodeworld@gmail.com>
2023-02-22 06:12:18 -0800

    9-times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,7 @@
 
 void times_table(void)
 {
+
 	int x, y, z, u, d;
 	for (x = 0; x  <= 9; x++)
 	{
@@ -20,7 +21,7 @@ void times_table(void)
 				_putchar(44);
 				_putchar(32);
 				_putchar(d + '0');
-				_putchar( u + '0');
+				_putchar(u + '0');
 		}
 			else
 			{
