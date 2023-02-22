8b7a40b2cbd2ff80233f37a20ea595dfa40a817e
jaycode2023 <jaycodeworld@gmail.com>
2023-02-22 11:39:48 -0800

    11-print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -28,15 +28,15 @@ void print_to_98(int n)
 						}
 						}
 						}
-						else
+					else
 						{
-						for (; n >= 98; n--)
+					for (; n >= 98; n--)
 						{
-						if (n == 98)
+					if (n == 98)
 						{
 						printf("%d", n);
 						printf("\n");
-						break;
+					break;
 						}
 						else
 						{
