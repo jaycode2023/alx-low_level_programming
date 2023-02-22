9b52550068456710db71369b0fce41dc00a6c624
jaycode2023 <jaycodeworld@gmail.com>
2023-02-22 14:20:59 -0800

    natura

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Lists all the natural numbers below 1024 (excluded)
- *        that are multiples of 3 or 5.
- *
- * Return: Always (success)
+ * main - prints the sum of all multiplies of 3 or 5 up to 1024
+ * Return: Always (Success)
  */
+
 int main(void)
 {
-	int i, z  = 0;
+	int i, z = 0;
 
-	while  (i < 1024)
+	while((i < 1024)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			z += 1;
-		}
-		i++;
+		z += i;
+
+	}
+	i++;
 	}
 	printf("%d\n", z);
 	return (0);
