436182df5cf392e288deeeb137e4c4831d22b60e
jaycode2023 <jaycodeworld@gmail.com>
2023-02-27 04:16:07 -0800

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,13 +15,13 @@ void print_number (int n)
 
 	if (n < 0)
 	{
-		n *= -1;
-		k = n;
-		_putchar('-');
+	n *= -1;
+	k = n;
+	_putchar('-');
 	}
 	k /= 10;
 	if (k != 0)
-		print_number(k);
+	print_number(k);
 	_putchar((unsigned int) n % 10 + '0');
 }
 
