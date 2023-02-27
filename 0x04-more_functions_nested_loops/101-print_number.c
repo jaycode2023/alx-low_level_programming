af8255f2dcbeb4cd800f056b6831d2f07bca003e
jaycode2023 <jaycodeworld@gmail.com>
2023-02-27 03:51:55 -0800

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,26 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_number - prints an integer
  * @n: integer to be printed
+ * Return: nothing !
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		n *= -1;
+		k = n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
-	}
+	} 
+		k /= 10;
+	
 
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
+	if (k != 0)
+	
+		print_number(k);
 
-	_putchar((n1 % 10) + '0');
+	_putchar((unsigned int) n % 10 + '0');
 }
-Footer
-© 2023 GitHub, Inc.
