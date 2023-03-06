0552da20a277b993e781e51f9c9d8572b71415cf
jaycode2023 <jaycodeworld@gmail.com>
2023-03-06 06:54:59 -0800

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,11 +7,14 @@
  * @n: bytes
  * Return: Alwys 0 (success)
  */
+
 char *_memset(char *s, char b, unsigned int n)
+
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
 	return (s);
+
 }
